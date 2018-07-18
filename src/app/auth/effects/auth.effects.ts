13c86e90c0d8ab3f5e2cdaa5adb512d37792dbfd
Jorge Cano <jorgeucano@gmail.com>
2018-07-18 13:53:44 -0300

    add effects for auth

---
+++ src/app/auth/effects/auth.effects.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Action } from '@ngrx/store';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Observable, of } from 'rxjs';
+import {catchError, map, mergeMap, tap} from 'rxjs/operators';
+
+import {
+  AuthActionTypes,
+  LoggedIn, LoggedUser,
+  LoginUser, LogoutAuth,
+  LoginUserError
+} from '../actions/auth.action';
+import {AuthService} from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthEffects {
+
+
+  @Effect()
+  LoginUserError$: Observable<Action> = this.actions$.pipe(
+    ofType<LoginUserError>(AuthActionTypes.LoginUserError),
+    tap(v => console.log('LoggedAPI error', v.payload)),
+    map (data => {
+      console.log('ERROR', data);
+      return { type: 'LOGIN_API_ERROR', payload: 'Email or password incorrect' };
+    })
+  );
+
+
+  // this efect redirect to home
+  @Effect()
+  LoginUser$: Observable<Action> = this.actions$.pipe(
+    ofType<LoginUser>(AuthActionTypes.LoginUser),
+    tap(v => console.log('loginUser effect', v)),
+    mergeMap(action =>
+      this.authService.login({ 
+          email: action.payload.user, 
+          username: '',
+          password: action.payload.pass})
+    )
+  );
+
+  @Effect()
+  LoggedUser$: Observable<Action> = this.actions$.pipe(
+    ofType<LoggedUser>(AuthActionTypes.LoggedUser),
+    tap(v => console.log('LoggedUser payload', v.payload)),
+    map( data => {
+      console.log(data);
+      return { type: '', payload: data };
+    })
+  );
+
+  constructor(private http: HttpClient, private actions$: Actions, private authService: AuthService) {}
+}
