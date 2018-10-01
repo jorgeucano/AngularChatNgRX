e1c377f688ff7b98e3d56126ce37b49ab35d47c9
Jorge Cano <jorgeucano@gmail.com>
2018-10-01 18:28:10 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCr93b0cRCdFIqxKWe6IxNcbkZLl2Xom3E",
-    authDomain: "functions-example-5a84c.firebaseapp.com",
-    databaseURL: "https://functions-example-5a84c.firebaseio.com",
-    projectId: "functions-example-5a84c",
-    storageBucket: "functions-example-5a84c.appspot.com",
-    messagingSenderId: "428402660430"
+    apiKey: "------",
+    authDomain: "------",
+    databaseURL: "------",
+    projectId: "------",
+    storageBucket: "------",
+    messagingSenderId: "------"
   }
 };
 
